fe8dd503c021238ca63d8e4df31964e9fb71fb6a
Yurly123 <joo7won@gmail.com>
2023-03-12 23:30:23 +0900

    Change to sveltestrap and add navbar

---
+++ src/lib/ui/header.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Navbar, NavbarBrand, Collapse, Nav, NavItem, NavLink, NavbarToggler } from 'sveltestrap';
+    import { base } from '$app/paths';
+
+    let isOpen = false
+    function toggle() {
+        isOpen = !isOpen
+    }
+</script>
+
+<Navbar color='dark' dark class='me-auto'>
+    <NavbarBrand href='{base}/'>HTML</NavbarBrand>
+    <NavbarToggler on:click={toggle} />
+    <Collapse {isOpen} navbar>
+        <Nav navbar>
+            <NavItem>
+                <NavLink href='{base}/tag'>태그</NavLink>
+            </NavItem>
+        </Nav>
+    </Collapse>
+</Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { Styles } from 'sveltestrap';
+    import { Header } from '$lib/ui';
+
     import { getTitleName } from '$lib/util';
     const title = getTitleName('HTML 문서')
 </script>
@@ -6,5 +9,9 @@
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
+
+<Styles></Styles>
+
+<Header></Header>
 
 <slot></slot>
