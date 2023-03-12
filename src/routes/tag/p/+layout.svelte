476c2a3c96ff951f06a82e883dd213abb4efc7c1
Yurly123 <joo7won@gmail.com>
2023-03-12 21:52:19 +0900

    Add p tag documentation and change base url

---
+++ src/routes/tag/p/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { getTitleName } from '$lib/util';
+    const title = getTitleName('p 태그')
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<slot></slot>
