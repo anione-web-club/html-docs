190986aca0e2e305d9f0a0812e3fa2a310aa2724
Yurly123 <joo7won@gmail.com>
2023-03-13 15:28:00 +0900

    Add example code component

---
+++ src/lib/ui/example-code.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {
+        Card,
+        CardBody,
+        CardHeader,
+        CardTitle,
+        Form,
+        FormGroup,
+        Input,
+    } from 'sveltestrap/src';
+</script>
+
+<Card>
+    <CardHeader>
+        <CardTitle>예제</CardTitle>
+    </CardHeader>
+    <CardBody>
+        <Form>
+            <FormGroup floating label='p 태그'>
+                <Input placeholder='예제' />
+            </FormGroup>
+        </Form>
+    </CardBody>
+</Card>
--- src/routes/tag/p/+page.svelte
+++ src/routes/tag/p/+page.svelte
@@ -1,19 +1,17 @@
+<script>
+    import { Badge } from 'sveltestrap/src';
+    import { ExampleCode } from '$lib/ui';
+    const example = '<p>훌륭한 코드는 훌륭한 문서보다 낫다.  (Steve McConnell, 개발자)</p>'
+</script>
+
 <h1>p</h1>
 
 <p>
-    HTML <code>{'<p>'}</code> 태그는 하나의 문단을 나타냅니다. 
+    HTML <Badge>{'<p>'}</Badge> 태그는 하나의 문단을 나타냅니다. 
     시각적인 매체에서, 문단은 보통 인접 블록과의 여백과 첫 줄의 들여쓰기로 구분하지만, 
     HTML에서 문단은 이미지나 입력 폼 등 서로 관련있는 콘텐츠 무엇이나 될 수 있습니다.
     <br>
-    문단은 블록 레벨 요소이며, 자신의 닫는 태그(<code>{'</p>'}</code>) 이전에 다른 블록 레벨 태그가 분석되면 자동으로 닫힙니다. 
+    문단은 블록 레벨 요소이며, 자신의 닫는 태그(<Badge>{'</p>'}</Badge>) 이전에 다른 블록 레벨 태그가 분석되면 자동으로 닫힙니다. 
 </p>
 
-<h3>예제:</h3>
-
-<code>
-    {`
-        <p>안녕하세요 여러분들</p>
-    `}
-</code>
-
-<p>안녕하세요 여러분들</p>
+<ExampleCode></ExampleCode>
